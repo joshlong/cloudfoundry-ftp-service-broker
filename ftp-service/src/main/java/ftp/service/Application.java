d2024fb9c67c273fb53743a54af8c8dd01d5fe4c
Josh Long <josh@joshlong.com>
2015-06-22 01:55:01 +0200

    polish

--- ftp-service/src/main/java/ftp/service/Application.java
+++ ftp-service/src/main/java/ftp/service/Application.java
@@ -1,7 +1,5 @@
 package ftp.service;
 
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
 import org.apache.ftpserver.filesystem.nativefs.NativeFileSystemFactory;
@@ -10,23 +8,25 @@ import org.apache.ftpserver.ftplet.FtpException;
 import org.apache.ftpserver.ftplet.UserManager;
 import org.apache.ftpserver.listener.Listener;
 import org.apache.ftpserver.listener.ListenerFactory;
-import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.util.Assert;
 
 import javax.sql.DataSource;
-import java.lang.reflect.Method;
 import java.util.Collections;
 
+
+/**
+ *
+ *  <p>This is the comment for this class</p>
+ *
+ *  @author Josh Long
+ *
+ */
 @SpringBootApplication
 public class Application {
 
